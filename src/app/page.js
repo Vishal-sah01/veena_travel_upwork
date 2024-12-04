bfd3cd5e0ea12ce2771db0a7bb15de858139d9b3
Vishal-sah01 <ivishalsah01oct@gmail.com>
2024-12-04 18:04:48 +0530

    add hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Navbar from '../components/Navbar';
 import Navbar2nd from '../components/Navbar2nd';
-
+import Hero from '../components/Hero';
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Navbar2nd />
+      <Hero />
     </main>
   );
 }
